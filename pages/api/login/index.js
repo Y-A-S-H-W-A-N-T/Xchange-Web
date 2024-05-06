d06ca7add55ce84f5a8fe5906bd7ccc835eae633
raoyashwant <raoyashwant132@gmail.com>
2024-05-07 03:51:18 +0530

    Shifted Login functionality from node backend to NEXTJS backend

---
+++ pages/api/login/index.js
@@ -0,0 +1,15 @@
+import connect from '../../../mongo'
+import User from '../../../Schema/UserSchema.js'
+
+
+export default async function POST(req,res) {
+    try{
+        await connect()
+        const Found = await User.find(req.body)
+        Found.length>0? res.status(200).json({message: 'User Found', name: Found[0].name, vtu: Found[0].vtu}) : res.json({status: 400,message: 'User Not Found'})
+    }
+    catch(err)
+    {
+        res.json(req.body.vtu)
+    }
+  }
--- pages/index.js
+++ pages/index.js
@@ -8,30 +8,32 @@ export default function Home() {
   const router = useRouter();
 
   const [student,setStudent] = useState({
-    vtu: '',
-    password: ''
+    "vtu": '',
+    "password": ''
   })
+
+  // const Login = async()=>{
+  //   if(student.vtu==='' || student.password==='')
+  //       return alert('Enter Details')
+  //   await axios.post('http://localhost:8000/login',student)
+  //   .then((res)=>{
+  //     if(res.data.message==='login_error') return
+  //     localStorage.setItem('name', JSON.stringify(res.data.name))
+  //     localStorage.setItem('vtu', JSON.stringify(res.data.vtu))
+  //     router.replace('/home')
+  //   })
+  // }
 
   const Login = async()=>{
     if(student.vtu==='' || student.password==='')
-        return alert('Enter Details')
-    await axios.post('http://localhost:8000/login',student)
+      return alert('Enter Details')
+    await axios.post('http://localhost:3000/api/login',student)
     .then((res)=>{
-      if(res.data.message==='login_error') return
-      localStorage.setItem('name', JSON.stringify(res.data.name))
-      localStorage.setItem('vtu', JSON.stringify(res.data.vtu))
-      router.replace('/home')
+      if(res.data.status===400) return alert("Wrong Credentials")
+        localStorage.setItem('name', JSON.stringify(res.data.name))
+        localStorage.setItem('vtu', JSON.stringify(res.data.vtu))
+        router.replace('/home')
     })
-  }
-
-  const Check = async()=>{
-    const data = await fetch('http://localhost:3000/',{
-      headers: {
-        Accept: "application/json",
-        method: "GET"
-      }
-    })
-    console.log("DATA -> ",data.json())
   }
 
   return (
@@ -43,9 +45,6 @@ export default function Home() {
                 <div className={login.submit}>
                   <p onClick={Login}>LOGIN</p>
                 </div>
-            </div>
-            <div onClick={Check}>
-              TRY NEXT BACKEND RESPONSE
             </div>
         </div>
     </div>
