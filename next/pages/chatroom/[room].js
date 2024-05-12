08945b8befab9a286218aaab962e469be160d731
raoyashwant <raoyashwant132@gmail.com>
2024-05-12 16:56:09 +0530

    Updated user and sender message styles

--- next/pages/chatroom/[room].js
+++ next/pages/chatroom/[room].js
@@ -1,72 +1,108 @@
-import React, { useEffect, useState } from 'react'
-import io from 'socket.io-client'
-import { useRouter } from 'next/router'
-import { storage } from "../../config"
-import { uploadBytes, ref, getDownloadURL } from "firebase/storage"
-import Image from 'next/image'
+import React, { useEffect, useState } from 'react';
+import io from 'socket.io-client';
+import { useRouter } from 'next/router';
+import { storage } from '../../config';
+import { uploadBytes, ref, getDownloadURL } from 'firebase/storage';
+import Image from 'next/image';
+import styles from '../../styles/room.module.css';
 
-const socket = io('http://localhost:8000/')
+const socket = io('http://localhost:8000/');
 
 export default function Home() {
+  const router = useRouter();
+  const { room } = router.query;
+  const [media, setMedia] = useState('');
+  const [vtu, setVtu] = useState(null);
+  const [oldMessages, setOldMessages] = useState([{}]);
+  const [message, setMessage] = useState('');
 
-  const router = useRouter()
-  const { room } = router.query
-  const [media,setMedia] = useState('')
-  const [vtu,setVtu] = useState(null)
-  const [oldMessages,setOldMessages] = useState([{}])
-  const [message,setMessage] = useState('')
+  useEffect(() => {
+    let vtu = localStorage.getItem('vtu');
+    const VTU = vtu.replace(/^"(.*)"$/, '$1');
+    setVtu(VTU);
+    socket.emit('connected', room);
+    socket.on('pastMessages', past => {
+      setOldMessages(past);
+      console.log(past);
+    });
+  }, [message]);
 
-    useEffect(() => {
-      let vtu = localStorage.getItem('vtu')
-      const VTU = vtu.replace(/^"(.*)"$/, '$1')
-      setVtu(VTU)
-        socket.emit('connected',room)
-        socket.on('pastMessages',past=>{
-          setOldMessages(past)
-          console.log(past)
-        })
+  const SEND = () => {
+    if(media==='' && message==='') return
+    socket.emit('chat', {
+      room_id: room,
+      message: message,
+      media: media,
+      sender_vtu: vtu,
+    });
+    setMessage('');
+  };
 
-    },[message]);
+  const handleImage = image => {
+    const Img_ref = ref(storage, `/messages/${vtu}/`);
+    uploadBytes(Img_ref, image).then(res => {
+      getDownloadURL(res.ref).then(async link => {
+        setMedia(link);
+      });
+    });
+  };
 
-    const SEND = ()=>{
-      socket.emit('chat',{
-        room_id: room,
-        message: message,
-        media: media,
-        sender_vtu: vtu
-      })
-      setMessage('')
-    }
+  console.log(media);
 
-    const handleImage = (image)=>{
-      const Img_ref = ref(storage,`/messages/${vtu}/`)
-        uploadBytes(Img_ref,image)
-        .then((res)=>{
-            getDownloadURL(res.ref)
-            .then(async(link)=>{
-                setMedia(link)
-            })
-        })
-    }
-
-    console.log(media)
-
-    return (
-      <div>
-        Socket.io <br/>
-        <input type='file' onChange={(e)=>handleImage(e.target.files[0])}/><label>Send an image</label>
-        {media && <Image src={media} width={50} height={50}/>}
-        <input placeholder='Type here' onChange={(e)=>setMessage(e.target.value)} value={message}/>
-        <button onClick={SEND}>SEND</button>
+  return (
+    <>
+    <div className={styles.chatContainer}>
+      <div style={{paddingBottom: '60px'}}>
+        {oldMessages.map((val, ind) =>{
+          if(vtu===val.sender_vtu)
+            {
+              return(
+                <div className={styles.message_box} style={{marginLeft: '50%'}}>
+                    <div key={ind} className={styles.messageContainer}>
+                      <div className={styles.message}>
+                        <p>
+                          {val.message}
+                        </p>
+                      </div>
+                      {val.media && <Image style={{marginLeft: '50%'}} src={val.media} width={200} height={200} />}
+                    </div>
+                </div>
+              )
+            }
+            else{
+              return(
+                <div className={styles.message_box}>
+                    <div key={ind} className={styles.messageContainer}>
+                      {val.sender_vtu}:
+                      <div className={styles.message} style={{width: '50%'}}>
+                        <p>
+                          {val.message}
+                        </p>
+                      </div>
+                      {val.media && <Image src={val.media} width={100} height={100} />}
+                    </div>
+                </div>
+              )
+            }
+        })}
+      </div>
+    </div>
+    <div className={styles.send_field}>
+        <div className={styles.inputContainer}>
+          <input type='file' onChange={e => handleImage(e.target.files[0])} id="fileInput"/>
+          <label htmlFor="fileInput">Send an image</label>
+          <input
+            placeholder='Type here'
+            onChange={e => setMessage(e.target.value)}
+            value={message}
+            type='text'
+          />
+          <button onClick={SEND}>SEND</button>
+        </div>
         <div>
-          {
-            oldMessages.map((val,ind)=>(
-              <div key={ind}>
-                  <p>{val.sender_vtu} : </p><p>{val.message} {val.media && <Image src={val.media} width={50} height={50}/>}</p>
-              </div>
-            ))
-          }
+          {media && <Image src={media} width={50} height={50} />}
         </div>
       </div>
-    )
+  </>
+  );
 }
