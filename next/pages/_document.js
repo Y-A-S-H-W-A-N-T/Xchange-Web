a199fc61b72b4a8a60f36c6e4dbf39b5c84472fc
raoyashwant <raoyashwant132@gmail.com>
2024-08-16 01:35:53 +0530

    Added headers

--- next/pages/_document.js
+++ next/pages/_document.js
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" href="/pic.png" />
+        <title>Xchange</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
